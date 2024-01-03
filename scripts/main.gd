95163e24a67b3e9d4218384fae01aa3ec11d30f1
Lucie Sundermeijer <lucie.sun@gmail.com>
2024-01-03 20:16:52 +0100

    add menu with keybord and mouse controls

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
-func _on_next_level_trigger_body_entered(body):
-	if body.is_in_group("player"):
-		call_deferred("go_to_next_level")
+@onready var start_button = $Control/VBoxContainer/start_button
 
-func go_to_next_level():
-	get_tree().change_scene_to_file("res://scenes/level_2.tscn")
+func _ready():
+	start_button.grab_focus()
 
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+func _on_quit_button_pressed():
+	get_tree().quit()
